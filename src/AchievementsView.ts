42991371c4259a7043a79be1bcf3c2bc95a908a5
Bastien DAVID <bastien.david@orange.com>
2019-09-10 14:22:26 +0200

    Added display of the graph of achievements
    Achievements are now displayed as a graph.
    The current version only displays some achievements, the graph has yet to be built.

--- src/AchievementsView.ts
+++ src/AchievementsView.ts
@@ -10,9 +10,37 @@ export class AchievementsView {
         let achievement2 = new Achievement(new AchievementsData().data[2], []);
         let achievement3 = new Achievement(new AchievementsData().data[3], []);
         achievement.unlocks.push(achievement1, achievement2, achievement3);
+        achievement.unlock();
+
+        const graphView = this.createGraphView(document);
+
+        const gridOfAchievements: Map<number, Achievement[]> = achievement.convertToUnlocksGrid();
+        gridOfAchievements.forEach((achievements: Achievement[]) => {
+            const graphRow = this.createGraphRow(document);
+            graphView.appendChild(graphRow);
+            achievements.forEach((achievement) => {
+                this.addAchievementToGraphRow(document, graphRow, achievement);
+            });
+        });
+
+        document.getElementById(containerId).appendChild(graphView);
+    }
 
 
+    private createGraphView(document: Document): HTMLElement {
+        let graphView = document.createElement('div');
+        graphView.classList.add('achievements-graph');
+        return graphView;
+    }
+
+    private createGraphRow(document: Document): HTMLElement {
+        let graphView = document.createElement('div');
+        graphView.classList.add('achievements-graph-row');
+        return graphView;
+    }
+
+    private addAchievementToGraphRow(document: Document, graphRow: HTMLElement, achievement: Achievement) {
         let view = new AchievementView(document, achievement);
-        document.getElementById(containerId).appendChild(view.getView());
+        graphRow.appendChild(view.getView());
     }
 }
